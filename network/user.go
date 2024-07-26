4caed931ecae79cca988da93c04795bab00d4a3e
Griotold <gotjd9773@naver.com>
2024-07-26 16:57:52 +0900

    API 구조 잡기

---
+++ network/user.go
@@ -0,0 +1,51 @@
+package network
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"sync"
+)
+
+var (
+	userRouterInit     sync.Once
+	userRouterInstance *userRouter
+)
+
+type userRouter struct {
+	router *Network
+	// service
+}
+
+func newUserRouter(router *Network) *userRouter {
+	userRouterInit.Do(func() {
+		userRouterInstance = &userRouter{
+			router: router,
+		}
+
+		router.registerGET("/", userRouterInstance.get)
+		router.registerPOST("/", userRouterInstance.create)
+		router.registerUPDATE("/", userRouterInstance.update)
+		router.registerDELETE("/", userRouterInstance.delete)
+
+	})
+
+	return userRouterInstance
+}
+
+// CRUD
+
+func (u *userRouter) create(c *gin.Context) {
+	fmt.Println("create 입니다.")
+}
+
+func (u *userRouter) get(c *gin.Context) {
+	fmt.Println("get 입니다.")
+}
+
+func (u *userRouter) update(c *gin.Context) {
+	fmt.Println("update 입니다.")
+}
+
+func (u *userRouter) delete(c *gin.Context) {
+	fmt.Println("delete 입니다.")
+}
